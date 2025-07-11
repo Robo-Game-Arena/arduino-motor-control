4223af30ec09684326c3c250363f4c17468076f3
nicholas-chung-cmu <149908770+nicholas-chung-cmu@users.noreply.github.com>
2025-07-11 16:59:22 -0400

    Update servo_characterization.ino

--- servo_characterization.ino
+++ servo_characterization.ino
@@ -5,47 +5,14 @@ Servo leftServo;
 Servo rightServo;
 
 // ——— Configuration ————————————————————————————————————————————————————
-const float WHEELBASE = 90.0;         // distance between wheels in millimeters
-const float WHEEL_RADIUS = 25.0;      //
-const float WHEEL_VEL_MAX = 500.0;    // max wheel velocity in millimeters/sec
-const float WHEEL_OMEGA_MAX = WHEEL_VEL_MAX / WHEEL_RADIUS;  // max wheel angular velocity in radians/sec
-
 const int LEFT_SERVO_PIN  = 5;    // ESP32 Feather pin for left continuous‐rotation servo
 const int RIGHT_SERVO_PIN = 4;    // ESP32 Feather pin for right continuous‐rotation servo
-
-const float OFFSETS[5] = {0.000, 0.250, 0.500, 0.750, 1.000};
-const float SCALE_L[5] = {0.000, 0.159, 0.324, 0.495, 1.000};
-const float SCALE_R[5] = {0.000, 0.116, 0.231, 0.410, 0.686};
-
 
 const int PWM_MIN    = 500;        // full reverse
 const int PWM_STOP   = 1500;       // stop
 const int PWM_MAX    = 2500;       // full forward
 
 float speed = 0.0; // percent
-
-int omegaToOffset(int raw, bool isLeft) {
-  float percent_offset = float(map(raw, -WHEEL_OMEGA_MAX, WHEEL_OMEGA_MAX, -1000, 1000)) / 1000;
-  float scale = 0;
-  for (int i = 0; i < 4; i++) {
-    if ((OFFSETS[i] <= abs(percent_offset)) & (abs(percent_offset) <= OFFSETS[i+1])) { 
-      float x = (percent_offset - OFFSETS[i]) / 0.25; 
-      if (isLeft) {
-        scale = SCALE_L[i] + x * (SCALE_L[i+1] - SCALE_L[i]);
-      }
-      else {
-        scale = SCALE_R[i] + x * (SCALE_R[i+1] - SCALE_R[i]);
-      }
-    }
-  }
-  Serial.println(scale);
-  if (raw > 0) { //positive
-    return int((PWM_MAX - PWM_STOP) * scale);
-  }
-  else {
-    return int((PWM_MIN - PWM_STOP) * scale);
-  }
-}
 
 void setup() {
     Serial.begin(115200);
@@ -70,8 +37,8 @@ void loop() {
     delay(1000);
   }
   speed = (speed + 0.25);
-  leftServo.writeMicroseconds(PWM_STOP + omegaToOffset(WHEEL_OMEGA_MAX*speed, true));
-  rightServo.writeMicroseconds(PWM_STOP - omegaToOffset(WHEEL_OMEGA_MAX*speed, false));     
+  leftServo.writeMicroseconds(PWM_STOP + (PWM_MAX - PWM_STOP) * speed);
+  rightServo.writeMicroseconds(PWM_STOP + (PWM_MAX - PWM_STOP) * speed);   
   delay(2000);
   leftServo.writeMicroseconds(PWM_STOP);
   rightServo.writeMicroseconds(PWM_STOP);
